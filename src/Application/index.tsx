79859ed5726e9a306610f49c1c463a746ed27326
wzalazar <walter.zalazar.mdp@gmail.com>
2020-06-27 18:13:12 -0300

    add global style

--- src/Application/index.tsx
+++ src/Application/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Reset } from 'styled-reset'
 
 import { Providers } from './Providers'
 import { Routes } from './Routes'
+import { GlobalStyles } from './GlobalStyles'
 
 export const Application = () => (
   <Providers>
+    <Reset />
+    <GlobalStyles />
     <Routes />
   </Providers>
 )
