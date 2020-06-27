2ac89990a672c9ea2b105fb242a12452bbd72646
wzalazar <walter.zalazar.mdp@gmail.com>
2020-06-27 18:13:32 -0300

    add new variables for the theme

--- src/Application/Theme.ts
+++ src/Application/Theme.ts
@@ -1,5 +1,9 @@
 export const Theme = {
   colors: {
+    black: '#1c1c1c',
+    white: '#FFF',
+    whiteGray: '#edeff1',
+    gray: '#DAE0E6',
     violet: 'rgb(112, 76, 182)',
     violetOpacity: (opacity: string) => `rgb(112, 76, 182, ${opacity})`,
   },
