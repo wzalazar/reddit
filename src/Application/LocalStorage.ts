8852fde21b6921b7778a002afc4dc7314d38ee4e
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2020-06-29 15:04:23 -0300

    feat: App state-preservation/restoration (#68)

---
+++ src/Application/LocalStorage.ts
@@ -0,0 +1,30 @@
+export const LocalStorage = () => {
+  const loadState = () => {
+    try {
+      const serializedData = localStorage.getItem('state')
+      if (serializedData === null) {
+        return undefined
+      }
+      return JSON.parse(serializedData)
+    } catch (error) {
+      // TODO handle error
+    }
+  }
+
+  const saveState = (state: any) => {
+    /* eslint-disable no-undef */
+    setImmediate(() => {
+      try {
+        let serializedData = JSON.stringify(state)
+        localStorage.setItem('state', serializedData)
+      } catch (error) {
+        // TODO handle error
+      }
+    }, 0)
+  }
+
+  return {
+    loadState,
+    saveState,
+  }
+}
--- src/Application/Store.ts
+++ src/Application/Store.ts
@@ -1,12 +1,22 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
 import postsReducer from 'Features/PostReddit/postsSlice'
 import appReducer from 'Features/App/appSlice'
+import { LocalStorage } from './LocalStorage'
+
+const localStorage = LocalStorage()
+
+const preloadedState = localStorage.loadState()
 
 export const store = configureStore({
+  preloadedState,
   reducer: {
     postsReddit: postsReducer,
     app: appReducer,
   },
+})
+
+store.subscribe(() => {
+  localStorage.saveState(store.getState())
 })
 
 export type RootState = ReturnType<typeof store.getState>
