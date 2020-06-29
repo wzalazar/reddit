a8dc708d486c76893c3ce8b30e5b8fa69f4e5ea1
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2020-06-29 18:02:25 -0300

    fix: Add Netlify redirect (#72)

--- src/Features/PostReddit/API/Reddit.ts
+++ src/Features/PostReddit/API/Reddit.ts
@@ -1,7 +1,7 @@
 export const RedditAPI = () => {
   const getPost = (after?: string): Promise<any> => {
     const url = new URL('https://www.reddit.com/top.json')
-    url.searchParams.set('limit', '10')
+    url.searchParams.set('limit', '50')
     after && url.searchParams.set('after', after)
 
     return fetch(url.href).then((data) => data.json())
